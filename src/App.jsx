bdc21276643abcd34510bce623a1e5ce90cd85b5
Wilson Ejulu <emox1995@gmail.com>
2024-07-01 12:50:20 +0300

    populate notes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((item) => (
+        <Note key={item.key} title={item.title} content={item.content} />
+      ))}
       <Footer></Footer>
     </div>
   );
